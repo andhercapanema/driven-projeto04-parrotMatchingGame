68e4815b699ca80bad7c6097d2e52d06105f4653
andherpaulo <andher.dev@gmail.com>
2022-08-22 16:27:15 -0300

    Adição de novos gifs e aumento da quantidade limite de cards no jogo

--- script.js
+++ script.js
@@ -26,6 +26,126 @@ const oneOfEachCard = [
     `<button class="unicorn c-card" onclick='turnCard(this)'>
         <img class="c-card__back" src="./img/back.png" alt="">
         <img class="c-card__front" src="./gifs/unicornparrot.gif" alt="">
+    </button>`,
+    `<button class="astronaut c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/astronautparrot.gif" alt="">
+    </button>`,
+    `<button class="beer c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/beerparrot.gif" alt="">
+    </button>`,
+    `<button class="birthday c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/birthdaypartyparrot.gif" alt="">
+    </button>`,
+    `<button class="ceiling c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/ceilingparrot.gif" alt="">
+    </button>`,
+    `<button class="christmas c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/christmasparrot.gif" alt="">
+    </button>`,
+    `<button class="conga c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/congaparrot.gif" alt="">
+    </button>`,
+    `<button class="disco c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/discoparrot.gif" alt="">
+    </button>`,
+    `<button class="everythingsfine c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/everythingsfineparrot.gif" alt="">
+    </button>`,
+    `<button class="exceptionallyfast c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/exceptionallyfastparrot.gif" alt="">
+    </button>`,
+    `<button class="github c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/githubparrot.gif" alt="">
+    </button>`,
+    `<button class="glimpse c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/glimpseparrot.gif" alt="">
+    </button>`,
+    `<button class="headbanging c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/headbangingparrot.gif" alt="">
+    </button>`,
+    `<button class="heading c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/headingparrot.gif" alt="">
+    </button>`,
+    `<button class="hmm c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/hmmparrot.gif" alt="">
+    </button>`,
+    `<button class="hypno c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/hypnoparrotlight.gif" alt="">
+    </button>`,
+    `<button class="invisible c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/invisibleparrot.gif" alt="">
+    </button>`,
+    `<button class="jedi c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/jediparrot.gif" alt="">
+    </button>`,
+    `<button class="jumping c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/jumpingparrot.gif" alt="">
+    </button>`,
+    `<button class="laptop c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/laptop_parrot.gif" alt="">
+    </button>`,
+    `<button class="moon c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/moonparrot.gif" alt="">
+    </button>`,
+    `<button class="party c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/partyparrot.gif" alt="">
+    </button>`,
+    `<button class="pirate c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/pirateparrot.gif" alt="">
+    </button>`,
+    `<button class="popcorn c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/popcornparrot.gif" alt="">
+    </button>`,
+    `<button class="portalblue c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/portalblueparrot.gif" alt="">
+    </button>`,
+    `<button class="pumpkin c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/pumpkinparrot.gif" alt="">
+    </button>`,
+    `<button class="quad c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/quadparrot.gif" alt="">
+    </button>`,
+    `<button class="sherlockholmes c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/sherlockholmesparrot.gif" alt="">
+    </button>`,
+    `<button class="sideways c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/sidewaysparrot.gif" alt="">
+    </button>`,
+    `<button class="sith c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/sithparrot.gif" alt="">
+    </button>`,
+    `<button class="wine c-card" onclick='turnCard(this)'>
+        <img class="c-card__back" src="./img/back.png" alt="">
+        <img class="c-card__front" src="./gifs/wineparrot.gif" alt="">
     </button>`
 ]
 
@@ -52,9 +172,9 @@ const game = document.querySelector('.c-matchingGame');
 
 const startGame = function () {
     // registrar número de cartas que o jogador quer
-    while (numberOfCards % 2 === 1 || numberOfCards < 4 || numberOfCards > 14) {
+    while (numberOfCards % 2 === 1 || numberOfCards < 4 || numberOfCards > 40) {
         numberOfCards = Number(prompt(`Com quantas cartas você quer jogar?
-Insira um número par de 4 a 14!`));
+Insira um número par de 4 a 40!`));
     }
 
     // distribuir as cartas em no mínimo duas linhas iguais
@@ -114,8 +234,10 @@ const checkEndGame = function () {
     if (turnedCards.length === numberOfCards) {
         clearInterval(counterId);
 
+        const oneOrMoreMinute = (displayedCounter.innerHTML.slice(0, 2) === '01') ? 'minuto' : 'minutos'
+
         alert(`Você ganhou em ${moves} jogadas!
-Seu tempo foi de ${displayedCounter.innerHTML.slice(0, 2)} minutos e ${displayedCounter.innerHTML.slice(3, 5)},${displayedCounter.innerHTML.slice(6, 8)} segundos!`);
+Seu tempo foi de ${displayedCounter.innerHTML.slice(0, 2)} ${(oneOrMoreMinute)} e ${displayedCounter.innerHTML.slice(3, 5)},${displayedCounter.innerHTML.slice(6, 8)} segundos!`);
 
         let playAgain = "";
         while (playAgain !== 'sim' && playAgain !== 'não') {
